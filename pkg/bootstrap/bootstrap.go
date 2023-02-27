9e828ca5f0c4496c1450e5dc276818566f297c47
MartinZitterkopf <martin.zitterkopf@gmail.com>
2023-02-27 10:40:48 -0300

    Se creo conexion a base de datos

---
+++ pkg/bootstrap/bootstrap.go
@@ -0,0 +1,41 @@
+package bootstrap
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/MartinZitterkopf/gocurse_domain/domain"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitLogger() *log.Logger {
+	return log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
+}
+
+func DBConnection() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_NAME"))
+
+	instanceDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if os.Getenv("DATABASE_DEBUG") == "true" {
+		instanceDB = instanceDB.Debug()
+	}
+
+	if os.Getenv("DATABASE_MIGRATE") == "true" {
+		if err := instanceDB.AutoMigrate(&domain.User{}); err != nil {
+			return nil, err
+		}
+	}
+
+	return instanceDB, nil
+}
