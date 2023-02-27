3e3f064570b9ed33186505ac23a8bbce05d0191c
MartinZitterkopf <martin.zitterkopf@gmail.com>
2023-02-27 14:54:40 -0300

    Refactor del codigo

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"strings"
@@ -11,12 +12,12 @@ import (
 
 type (
 	Repository interface {
-		Create(user *domain.User) error
-		GetAll(filters Fillters, limit, offset int) ([]domain.User, error)
-		Get(id string) (*domain.User, error)
-		Delete(id string) error
-		Update(id string, firstName *string, lastName *string, email *string, phone *string) error
-		Count(filters Fillters) (int, error)
+		Create(ctx context.Context, user *domain.User) error
+		GetAll(ctx context.Context, filters Fillters, limit, offset int) ([]domain.User, error)
+		Get(ctx context.Context, id string) (*domain.User, error)
+		Delete(ctx context.Context, id string) error
+		Update(ctx context.Context, id string, firstName *string, lastName *string, email *string, phone *string) error
+		Count(ctx context.Context, filters Fillters) (int, error)
 	}
 
 	repo struct {
@@ -32,53 +33,55 @@ func NewRepo(log *log.Logger, db *gorm.DB) Repository {
 	}
 }
 
-func (repo *repo) Create(user *domain.User) error {
+func (repo *repo) Create(ctx context.Context, user *domain.User) error {
 
-	if err := repo.db.Create(user).Error; err != nil {
-		repo.log.Printf("error; %v", err)
+	if err := repo.db.WithContext(ctx).Create(user).Error; err != nil {
+		repo.log.Println(err)
 		return err
 	}
 	repo.log.Println("User created with id: ", user.ID)
 	return nil
 }
 
-func (repo *repo) GetAll(filters Fillters, offset, limit int) ([]domain.User, error) {
+func (repo *repo) GetAll(ctx context.Context, filters Fillters, offset, limit int) ([]domain.User, error) {
 	var u []domain.User
 
-	// Model hace referencia al modelo de usuario y Find lo que hace es poblar la informacion que saca de la estructura
-	tx := repo.db.Model(&u)
+	tx := repo.db.WithContext(ctx).Model(&u)
 	tx = applyFilters(tx, filters)
 	tx = tx.Limit(limit).Offset(offset)
 
 	result := tx.Order("created_at desc").Find(&u)
 	if result.Error != nil {
+		repo.log.Println(result.Error)
 		return nil, result.Error
 	}
 
 	return u, nil
 }
 
-func (repo *repo) Get(id string) (*domain.User, error) {
+func (repo *repo) Get(ctx context.Context, id string) (*domain.User, error) {
 	user := domain.User{ID: id}
 
-	if err := repo.db.First(&user).Error; err != nil {
+	if err := repo.db.WithContext(ctx).First(&user).Error; err != nil {
+		repo.log.Println(err)
 		return nil, err
 	}
 
 	return &user, nil
 }
 
-func (repo *repo) Delete(id string) error {
+func (repo *repo) Delete(ctx context.Context, id string) error {
 	user := domain.User{ID: id}
 
-	if err := repo.db.Delete(&user).Error; err != nil {
+	if err := repo.db.WithContext(ctx).Delete(&user).Error; err != nil {
+		repo.log.Println(err)
 		return err
 	}
 
 	return nil
 }
 
-func (repo *repo) Update(id string, firstName *string, lastName *string, email *string, phone *string) error {
+func (repo *repo) Update(ctx context.Context, id string, firstName *string, lastName *string, email *string, phone *string) error {
 	values := make(map[string]interface{})
 
 	if firstName != nil {
@@ -97,14 +100,14 @@ func (repo *repo) Update(id string, firstName *string, lastName *string, email *
 		values["phone"] = *phone
 	}
 
-	if err := repo.db.Model(&domain.User{}).Where("id = ?", id).Updates(values).Error; err != nil {
+	if err := repo.db.WithContext(ctx).Model(&domain.User{}).Where("id = ?", id).Updates(values).Error; err != nil {
+		repo.log.Println(err)
 		return err
 	}
 
 	return nil
 }
 
-// recibe la base de datos desde gorm y el filtro a buscar
 func applyFilters(tx *gorm.DB, filters Fillters) *gorm.DB {
 	if filters.FirstName != "" {
 		filters.FirstName = fmt.Sprintf("%%%s%%", strings.ToLower(filters.FirstName))
@@ -119,12 +122,13 @@ func applyFilters(tx *gorm.DB, filters Fillters) *gorm.DB {
 	return tx
 }
 
-func (repo *repo) Count(filters Fillters) (int, error) {
+func (repo *repo) Count(ctx context.Context, filters Fillters) (int, error) {
 	var count int64
-	tx := repo.db.Model(domain.User{})
+	tx := repo.db.WithContext(ctx).Model(domain.User{})
 	tx = applyFilters(tx, filters)
 
 	if err := tx.Count(&count).Error; err != nil {
+		repo.log.Println(err)
 		return 0, err
 	}
 
