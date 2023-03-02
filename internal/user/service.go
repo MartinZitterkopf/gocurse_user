c55ef12e9303dc901b2ee19e3d98321318d972ae
MartinZitterkopf <martin.zitterkopf@gmail.com>
2023-03-02 15:33:42 -0300

    Refactor

--- internal/user/service.go
+++ internal/user/service.go
@@ -36,7 +36,8 @@ func NewService(log *log.Logger, repo Repository) Service {
 }
 
 func (s service) Create(ctx context.Context, firstName, lastName, email, phone string) (*domain.User, error) {
-	s.log.Println("Create user service")
+
+	s.log.Println("create user service")
 	user := domain.User{
 		FirstName: firstName,
 		LastName:  lastName,
@@ -53,6 +54,7 @@ func (s service) Create(ctx context.Context, firstName, lastName, email, phone s
 }
 
 func (s service) GetAll(ctx context.Context, filters Fillters, offset, limit int) ([]domain.User, error) {
+
 	users, err := s.repo.GetAll(ctx, filters, offset, limit)
 	if err != nil {
 		s.log.Println(err)
@@ -63,6 +65,7 @@ func (s service) GetAll(ctx context.Context, filters Fillters, offset, limit int
 }
 
 func (s service) GetByID(ctx context.Context, id string) (*domain.User, error) {
+
 	user, err := s.repo.GetByID(ctx, id)
 	if err != nil {
 		s.log.Println(err)
@@ -73,11 +76,21 @@ func (s service) GetByID(ctx context.Context, id string) (*domain.User, error) {
 }
 
 func (s service) Delete(ctx context.Context, id string) error {
-	return s.repo.Delete(ctx, id)
+
+	if err := s.repo.Delete(ctx, id); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s service) Update(ctx context.Context, id string, firstName *string, lastName *string, email *string, phone *string) error {
-	return s.repo.Update(ctx, id, firstName, lastName, email, phone)
+
+	if err := s.repo.Update(ctx, id, firstName, lastName, email, phone); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s service) Count(ctx context.Context, filters Fillters) (int, error) {
